aaac0b58d005c1dd156ea3ce5e4655ecf4a660cf
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-06-04 12:12:20 -0300

    Header component

--- front-web/src/app/app-routing.module.ts
+++ front-web/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardPageComponent}
+  {path: '', component: DashboardPageComponent}
 ];
 
 @NgModule({
